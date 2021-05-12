3a08cf85d4bd84b1c48b56a919fe47e8b8730f48
0Supa <36031171+0Supa@users.noreply.github.com>
2021-05-12 16:09:56 +0300

    Update polly.js

--- routes/polly.js
+++ routes/polly.js
@@ -9,27 +9,15 @@ const router = express.Router();
 router.get('/polly', async function (req, res) {
     const retryCount = 3
     const phrase = req.query.text
-    const voice = req.query.voice
+    const voice = req.query.lang || req.query.voice
 
     if (!req.headers.authorization || req.headers.authorization !== auth) return res.status(401).redirect(`https://youtu.be/d1zzvW2oYjs`)
     if (!voice) return res.status(400).send({ error: { message: "You need to specify a voice" } })
     if (!phrase) return res.status(400).send({ error: { message: "You need to specify a phrase" } })
-    if (phrase.length > 1500) return res.status(414).send({ error: { message: "Text should be shorter than 1500 characters" } })
+    if (phrase.length > 600) return res.status(414).send({ error: { message: "Text should be shorter than 600 characters" } })
 
     try {
-        const { body } = await got.post('https://streamlabs.com/polly/speak', {
-            retryCount,
-            json: {
-                voice,
-                text: phrase
-            }
-        })
-
-        const json = JSON.parse(body)
-
-        if (req.query.directLink) return res.send({ url: json.speak_url })
-
-        const data = await got(json.speak_url, { retryCount }).buffer()
+        const data = await got(`https://api.streamelements.com/kappa/v2/speech?voice=${encodeURIComponent(voice)}&text=${encodeURIComponent(phrase)}`, { retryCount }).buffer()
 
         send(data, res, req, req.query.direct)
     } catch (err) {
