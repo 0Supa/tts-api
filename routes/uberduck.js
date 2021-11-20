ad94ab68249f66da2b34d249684e3948fd8a5f03
Supa <36031171+0Supa@users.noreply.github.com>
2021-11-20 17:00:11 +0200

    Update uberduck.js

--- routes/uberduck.js
+++ routes/uberduck.js
@@ -7,10 +7,9 @@ const got = require('got')
 const express = require("express");
 const router = express.Router();
 
-router.get('/uberduck', async function (req, res) {
-    const phrase = req.query.text
-    const voice = req.query.lang || req.query.voice
+app.use(express.json());
 
+async function handle(req, res, phrase, voice) {
     if (!req.headers.authorization || req.headers.authorization !== auth) return res.status(401).redirect(`https://youtu.be/d1zzvW2oYjs`)
     if (!voice) return res.status(400).send({ error: { message: "You need to specify a voice" } })
     if (!phrase) return res.status(400).send({ error: { message: "You need to specify a phrase" } })
@@ -19,12 +18,30 @@ router.get('/uberduck', async function (req, res) {
     try {
         const uuid = await uberduck.queue(voice, phrase)
         const tts = await uberduck.getResult(uuid)
+        if (req.query.direct) return res.send({ url: tts.path })
+
         const data = await got(tts.path).buffer()
 
-        send(data, res, req, req.query.direct)
+        send(data, res, req, false)
     } catch (err) {
         res.status(500).send({ error: { message: err } })
     }
+}
+
+router.get('/uberduck', async function (req, res) {
+    const phrase = req.query.text
+    const voice = req.query.lang || req.query.voice
+
+    await handle(res, req, phrase, voice)
+
+    logger.info(`Received Uberduck for ${phrase}`);
+});
+
+router.post('/uberduck', async function (req, res) {
+    const phrase = req.body.text
+    const voice = req.body.lang || req.body.voice
+
+    await handle(res, req, phrase, voice)
 
     logger.info(`Received Uberduck for ${phrase}`);
 });
